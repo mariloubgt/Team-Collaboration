f4e678d6ec1b42be262e60a93229971b3982117d
Aisha Hadjou <148077331+Aishaolah@users.noreply.github.com>
2024-12-27 03:19:39 +0100

    Update perceptron.c

--- src/perceptron.c
+++ src/perceptron.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+//Inputs in this case: Weather, Soil, Resources
+#define INPUTS 3 
+
+//Defining the structure 
+typedef struct {
+    double weights[INPUTS];
+    double bias;
+} Perceptron;
