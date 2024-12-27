6ddcc235f2f88d5e52a0838a21f4e1787545c5db
Aisha Hadjou <148077331+Aishaolah@users.noreply.github.com>
2024-12-27 10:22:04 +0100

    Update perceptron.c

--- src/perceptron.c
+++ src/perceptron.c
@@ -6,7 +6,25 @@
 #define INPUTS 3 
 
 //Defining the structure 
-typedef struct {
+typedef struct
+{
     double weights[INPUTS];
     double bias;
 } Perceptron;
+
+//Step activation function
+int step_function(double sum)
+{
+    return (sum >= 0) ? 1 : 0; 
+}
+
+//Calculating the weighted sum of inputs "net_input"
+double net_input (const Perceptron *perceptron, const double *inputs) 
+{
+    double sum=0;
+    for (int i=0; i <NUM_INPUTS; i++) {
+        sum += perceptron-> weights[i] * inputs[i];
+    }
+    sum += perceptron-> bias;
+    return sum;
+}    
