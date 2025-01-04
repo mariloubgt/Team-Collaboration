121776dfb58a621c2f769069bffa837f0ccb1d8d
Aisha Hadjou <148077331+Aishaolah@users.noreply.github.com>
2025-01-04 11:18:02 +0100

    Update perceptron.c

--- src/perceptron.c
+++ src/perceptron.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-//Inputs in this case: Weather, Soil, Resources
-#define INPUTS 3 
+#define NUM_INPUTS 3   // Number of input features (Weather, Soil, Resources)
+#define NUM_SAMPLES 100 // Number of data samples
 
-//Defining the structure 
-typedef struct
-{
-    double weights[INPUTS];
+typedef struct {
+    double weights[NUM_INPUTS];
     double bias;
 } Perceptron;
-
-//Step activation function
-int step_function(double sum)
-{
-    return (sum >= 0) ? 1 : 0; 
-}
-
-//Calculating the weighted sum of inputs "net_input"
-double net_input (const Perceptron *perceptron, const double *inputs) 
-{
-    double sum=0;
-    for (int i=0; i <NUM_INPUTS; i++) {
-        sum += perceptron-> weights[i] * inputs[i];
-    }
-    sum += perceptron-> bias;
-    return sum;
-}    
+ 
