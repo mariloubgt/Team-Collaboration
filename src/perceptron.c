a85bd404dce1a77b68d22636ba5f38434f995c89
Aisha Hadjou <148077331+Aishaolah@users.noreply.github.com>
2025-01-04 11:28:04 +0100

    Update perceptron.c

--- src/perceptron.c
+++ src/perceptron.c
@@ -3,11 +3,17 @@
 #include <time.h>
 #include <string.h>
 
-#define NUM_INPUTS 3   // Number of input features (Weather, Soil, Resources)
-#define NUM_SAMPLES 100 // Number of data samples
+//Number of input features: Weather, Soil, Resources
+#define NUM_INPUTS 3 
+//Number of data samples (optional)
+#define NUM_SAMPLES 100 
 
 typedef struct {
     double weights[NUM_INPUTS];
     double bias;
 } Perceptron;
  
+//Activation function (step function)
+int step_function(double net_input) {
+    return (net_input >= 0) ? 1 : 0;
+}
