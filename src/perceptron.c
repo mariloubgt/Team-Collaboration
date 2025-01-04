dca89acce6c65c3e10ab3a11ae48bc241a03ab68
mariloubgt <bouguettayamarya@gmail.com>
2025-01-04 23:27:08 +0100

    added the net_imput function

--- src/perceptron.c
+++ src/perceptron.c
@@ -3,10 +3,20 @@
 #include <time.h>
 #include <string.h>
 
-#define NUM_INPUTS 2   
-#define NUM_SAMPLES 100 
+#define NUM_INPUTS 2   // Number of input features (Weather, Resources)
+#define NUM_SAMPLES 100 // Number of data samples
 
 typedef struct {
     double weights[NUM_INPUTS];
     double bias;
 } Perceptron;
+
+
+double net_input(const Perceptron *perceptron, const double *inputs) {
+    double sum = 0.0;
+    for (int i = 0; i < NUM_INPUTS; ++i) {
+        sum += perceptron->weights[i] * inputs[i];
+    }
+    sum += perceptron->bias;
+    return sum;
+}
