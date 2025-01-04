75ea4dd2c6f359ba06b0f7b68e069f5d09d9445a
mariloubgt <bouguettayamarya@gmail.com>
2025-01-04 23:13:46 +0100

    update

--- src/perceptron.c
+++ src/perceptron.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
+#define NUM_INPUTS 2   
+#define NUM_SAMPLES 100 
+
+typedef struct {
+    double weights[NUM_INPUTS];
+    double bias;
+} Perceptron;
