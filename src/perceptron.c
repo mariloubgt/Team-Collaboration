66606f846fae2bb9fe5449365c8bb6898de85360
mariloubgt <bouguettayamarya@gmail.com>
2025-01-04 23:35:19 +0100

    added the train_perceptron function

--- src/perceptron.c
+++ src/perceptron.c
@@ -20,3 +20,23 @@ double net_input(const Perceptron *perceptron, const double *inputs) {
     sum += perceptron->bias;
     return sum;
 }
+
+// Train the Perceptron
+void train_perceptron(Perceptron *perceptron,
+                      const double inputs[][NUM_INPUTS],
+                      const int *targets,
+                      size_t num_samples,
+                      double learning_rate,
+                      int max_epochs) {
+    for (int epoch = 0; epoch < max_epochs; ++epoch) {
+        for (size_t i = 0; i < num_samples; ++i) {
+            int prediction = predict(perceptron, inputs[i]);
+            double error = targets[i] - prediction;
+
+            // Update weights and bias
+            for (int j = 0; j < NUM_INPUTS; ++j) {
+                perceptron->weights[j] += learning_rate * error * inputs[i][j];
+            }
+            perceptron->bias += learning_rate * error;
+        }
+    }}
